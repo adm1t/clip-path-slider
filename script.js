b357e070f83ec63839101f61a3efdad3f8a5b751
adm1t <filippov95@mail.ru>
2019-11-06 14:35:15 +0300

    major upd
    . code refactoring
    . styles changes
    . image randomizing
    
    and several more things..

--- script.js
+++ script.js
@@ -3,88 +3,83 @@ let prevImg = {val: ""}, nextImg = {val: ""}, currentImg;
 
 window.addEventListener('load', function() {
 
-    let promise = new Promise((resolve) => {
+	let promise = new Promise((resolve) => {
 
-        images = document.querySelectorAll(".img-container img");
-        updImgClasses(images[getRandomInt(0, images.length - 1)]);
-        resolve();
+		images = document.querySelectorAll(".slide");
+		updImgClasses(images[0]);
+		resolve();
+	});
 
-    });
-    promise.then(()=> {
+	promise.then(() => {
 
-        arrows = [
-            {
-                element: document.querySelector(".arrow.left-arrow"),
-                curImgTempClassname: "prev-left-item",
-                newImgTempClassname: "current-right-item",
-                newImg: nextImg,
-                unusedImg: prevImg
-            },
-            {
-                element: document.querySelector(".arrow.right-arrow"),
-                curImgTempClassname: "prev-right-item",
-                newImgTempClassname: "current-left-item",
-                newImg: prevImg,
-                unusedImg: nextImg
-            }
-        ];
+		arrows = [
+			{
+				element: document.querySelector(".arrow.arrow_prev"),
+				curImgTempClassname: "slide_prev-left",
+				newImgTempClassname: "slide_current-right",
+				newImg: nextImg,
+				unusedImg: prevImg
+			},
+			{
+				element: document.querySelector(".arrow.arrow_next"),
+				curImgTempClassname: "slide_prev-right",
+				newImgTempClassname: "slide_current-left",
+				newImg: prevImg,
+				unusedImg: nextImg
+			}
+		];
 
-        arrows.forEach((item) => {
-            item.element.addEventListener('click', ()=>{
-                arrowClickHandler(item);
-            });
-        });
-    });
+		arrows.forEach((item) => {
+			item.element.addEventListener('click', () => {
+				arrowClickHandler(item);
+			});
+		});
+	});
 });
-
-function getRandomInt(min, max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 function updImgClasses(curImg){
 
-    currentImg = curImg;
-    currentImg.classList = "current-img";
+	currentImg = curImg;
+	currentImg.setAttribute("class", "slide slide_current");
 	
-    switch(curImg){
+	switch(curImg){
 
-        case images[0]:
-            prevImg.val = images[images.length - 1];
-            nextImg.val = curImg.nextElementSibling;
-            break;
+		case images[0]:
+			prevImg.val = images[images.length - 1];
+			nextImg.val = curImg.nextElementSibling;
+			break;
 		
-	case images[images.length - 1]:
-	    prevImg.val = curImg.previousElementSibling;
-	    nextImg.val = images[0];
-	    break;
-			
-	default:
-	    prevImg.val = curImg.previousElementSibling;
-	    nextImg.val = curImg.nextElementSibling;
-    }
+		case images[images.length - 1]:
+			prevImg.val = curImg.previousElementSibling;
+			nextImg.val = images[0];
+			break;
+				
+		default:
+			prevImg.val = curImg.previousElementSibling;
+			nextImg.val = curImg.nextElementSibling;
+	}
 
-    prevImg.val.classList = "next-left-img";
-    nextImg.val.classList = "next-right-img";
-
+	prevImg.val.setAttribute("class", "slide slide_next-left");
+	nextImg.val.setAttribute("class", "slide slide_next-right");
 };
 
 function arrowClickHandler(thisArrow){
 
-    document.querySelectorAll(".arrow").forEach((item) => {
-        item.classList.add("arrow-disabled");
-    });
+	document.querySelectorAll(".arrow").forEach((item) => {
+		item.classList.add("arrow_disabled");
+	});
 
-    currentImg.classList.add(thisArrow.curImgTempClassname);
-    thisArrow.newImg.val.classList.add(thisArrow.newImgTempClassname);
+	currentImg.classList.add(thisArrow.curImgTempClassname);
+	thisArrow.newImg.val.classList.add(thisArrow.newImgTempClassname);
 
-    setTimeout(() => {
-        currentImg.classList = "";
-        thisArrow.unusedImg.val.classList = "";
-        updImgClasses(thisArrow.newImg.val);
+	setTimeout(() => {
+		currentImg.classList = "";
+		thisArrow.unusedImg.val.setAttribute("class", 'slide');
+		updImgClasses(thisArrow.newImg.val);
 
-        document.querySelectorAll(".arrow").forEach((item) => {
-            item.classList.remove("arrow-disabled");
-        });
+		document.querySelectorAll(".arrow").forEach((item) => {
+			item.classList.remove("arrow_disabled");
+		});
 
-    }, 500);
+	}, 500);
 };
